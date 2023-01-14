caad07194d448b62f4103d778a62b42c51a5e00f
lst97 <laisiotou1997@gmail.com>
2023-01-14 18:51:22 +1100

    build: revert

--- src/app/app.py
+++ src/app/app.py
@@ -2,10 +2,14 @@ from flask import Flask, request
 import pymongo
 from flask_cors import CORS
 import json
+<<<<<<< HEAD
 from bson import json_util, ObjectId
 from dotenv import load_dotenv
 import os
 from os.path import join, dirname
+=======
+from bson import json_util
+>>>>>>> parent of 089802b (build: group records route)
 
 load_dotenv(dotenv_path=join(dirname(__file__), 'config/.env'))
 app = Flask(__name__)
@@ -33,34 +37,6 @@ def groups():
 
         # convert cursor to JSON string
         return json.dumps(list(cursor), default=json_util.default)
-
-
-@app.route('/group_records/<string:group_id>', methods=['GET'])
-def group_records(group_id):
-    if request.method == "GET":
-        db = establish_connection()
-        groups_collection = db.groups
-
-        cursor = groups_collection.find(
-            {"_id": ObjectId(group_id)}, {"records": 1})
-
-        records = []
-        for doc in cursor:
-            records.append(doc)
-
-        response = []
-        for record in records[0]["records"]:
-            record_obj = {}
-            record_obj["merchant_name"] = record["receipts"][0]["merchant_name"]
-            record_obj["receipt_no"] = record["receipts"][0]["receipt_no"]
-            record_obj["date"] = record["receipts"][0]["date"]
-            record_obj["payer"] = record["payer"]
-            record_obj["total"] = record["receipts"][0]["total"]
-            record_obj["payment_method"] = record["receipts"][0]["payment_method"]
-            record_obj["share_with"] = record["share_with"]
-            response.append(record_obj)
-
-        return json.dumps(response, default=json_util.default)
 
 
 @app.route('/groups_info', methods=['GET'])
@@ -92,4 +68,4 @@ def recipt():
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
