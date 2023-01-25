1dfc284135dfc62dcc591a00a1a1921c656d1b97
lst97 <laisiotou1997@gmail.com>
2023-01-25 20:09:47 +1100

    build: refactor upload function

--- src/api/receipt_parser/receipt_parser.py
+++ src/api/receipt_parser/receipt_parser.py
@@ -6,10 +6,15 @@ from os.path import join, dirname
 import sys
 import coloredlogs
 import json
+import os
 
 load_dotenv(dotenv_path=join(dirname(dirname(__file__)), 'config/.env'))
 
 LOGGER = logging.getLogger("receipt_parser.driver")
+
+IMAGE_FOLDER = 'data/img'
+TEMP_IMAGE_FOLDER = 'data/tmp'
+OCR_TEXT_FOLDER = 'data/txt'
 
 
 def run(files_name):
@@ -17,12 +22,17 @@ def run(files_name):
     enhancer.run(files_name)
     LOGGER.info("Start parsing OCR data...")
     receipts = parser.run(files_name)
+
     for idx, receipt in enumerate(receipts):
         receipt["file_name"] = files_name[idx]
         receipt["receipt_no"] = ''
 
-    sys.stdout.write(json.dumps(receipts).replace("\n", ""))
+    for file_name in files_name:
+        os.remove(join(dirname(__file__), IMAGE_FOLDER, file_name))
+        os.remove(join(dirname(__file__), TEMP_IMAGE_FOLDER, file_name))
+        os.remove(join(dirname(__file__), OCR_TEXT_FOLDER, file_name + '.txt'))
 
+    sys.stdout.write(json.dumps(receipts).replace("\n", ""))
     LOGGER.info("Parsing DONE.")
 
 
--- src/api/srm_api.py
+++ src/api/srm_api.py
@@ -3,7 +3,6 @@ from .srm_db import *
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json
-from bson import json_util
 from dotenv import load_dotenv
 import os
 from os.path import join, dirname
@@ -16,6 +15,7 @@ import imagehash
 from PIL import Image
 import io
 import re
+import concurrent.futures
 
 
 load_dotenv(dotenv_path=join(dirname(__file__), 'config/.env'))
@@ -66,15 +66,13 @@ def on_exit():
     LOGGER.info("on_exit() callback EXECUTED.")
 
 
-parser_output_string = ""
-
-
-def execute_receipt_parser(request_id) -> threading.Thread:
+def execute_receipt_parser(files_name):
     """
     Executes the receipt parser script as a separate thread, and
     calls the provided on_exit callback when the script exits.
     """
-    def receipt_parser_thread():
+
+    def receipt_parser():
         """
         Thread function that runs the receipt parser script and captures its output.
         """
@@ -83,27 +81,29 @@ def execute_receipt_parser(request_id) -> threading.Thread:
             LOGGER.info("Get files info from API")
             subprocess_command = ["python3",
                                   "./src/api/receipt_parser/receipt_parser.py"]
-            files_name = get_files_from_api(request_id)
             subprocess_command.append(str(len(files_name)))
             for file_name in files_name:
                 subprocess_command.append(file_name)
 
-            global parser_output_string
             parser_output_string = subprocess.check_output(
                 subprocess_command, encoding='utf-8')
+
+            on_exit()
+            return parser_output_string
 
         except (OSError, subprocess.CalledProcessError) as exception:
             LOGGER.error('Exception occured: ' + str(exception))
             LOGGER.info('Receipt Parser failed to complete it execution.')
             return False
-        else:
-            # notify API to find the result in data/txt
-            on_exit()
-        return True
 
-    parser_thread = threading.Thread(target=receipt_parser_thread)
-    parser_thread.start()
-    return parser_thread
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future = executor.submit(receipt_parser)
+        return future.result()
+
+    # parser_thread = threading.Thread(target=receipt_parser)
+    # parser_thread.start()
+    # parser_thread.join()
+    # return parser_output_string
 
 
 @app.route('/groups', methods=['POST', 'GET'])
@@ -165,47 +165,274 @@ def recipt(id):
 
 # For testing
 
+# @app.route('/test/upload/<string:group_id>', methods=['POST'])
+# def handle_upload(group_id):
+#     # how many files are expected to be uploaded for this request_id
+#     total_files = int(request.form.get('total_files'))
+#     request_id = request.form.get('request_id')
 
-upload_requests = {}
+#     if request_id not in upload_requests:
+#         upload_requests[request_id] = {
+#             "request_id": request_id, "total_files": total_files, "remaining": total_files, "files": [], "hashs": []}
+
+#     if not re.match(UUID_REGEX, request_id):
+#         upload_requests[request_id]["remaining"] -= 1
+#         return jsonify({"message": "Invalid request_id."}), 400
+
+#     image_file = request.files.get("file")
+
+#     if not re.match(FILE_NAME_REGEX, image_file.filename):
+#         upload_requests[request_id]["remaining"] -= 1
+#         return jsonify({"message": "Invalid file name."}), 400
+
+#     # image hash
+#     image_bytes = image_file.read()
+#     try:
+#         image = Image.open(io.BytesIO(image_bytes))
+#     except IOError:
+#         upload_requests[request_id]["remaining"] -= 1
+#         return jsonify({"message": "Invalid image file."}), 400
+
+#     image_hash = str(imagehash.average_hash(image))
+
+#     if find_image_hash(image_hash) is not None:
+#         upload_requests[request_id]["remaining"] -= 1
+#         LOGGER.info("Duplicated image found.")
+#         return jsonify({"message": "Duplicated image"}), 400
+
+#     # convert image to base64
+#     image_base64 = base64.b64encode(image_bytes).decode('utf-8')
+
+#     # save image file to disk
+#     try:
+#         with open(join(dirname(__file__), 'receipt_parser/data/img', image_file.filename), "wb") as stream:
+#             stream.write(base64.b64decode(image_base64))
+#     except Exception as e:
+#         LOGGER.error(e)
+
+#     # append the file name and base64 to the current record
+#     upload_requests[request_id]["files"].append({
+#         "file_name": image_file.filename,
+#         "base64": image_base64,
+#         "hash": image_hash})
+
+#     upload_requests[request_id]["remaining"] -= 1
+
+#     # if no remaining files, execute receipt parser
+#     if upload_requests[request_id]["remaining"] == 0:
+
+#         # add records to parse_queue db
+#         insert_parse_queue(
+#             upload_requests[request_id]["request_id"],
+#             group_id,
+#             upload_requests[request_id]["files"],
+#         )
+
+#         parser_thread = execute_receipt_parser(request_id)
+#         parser_thread.join()
+#         receipts_json_string = parser_output_string.splitlines()[-1]
+#         receipts = json.loads(receipts_json_string)
+
+#         # TODO: optmize
+#         for image_file in upload_requests[request_id]["files"]:
+#             for receipt in receipts:
+#                 if image_file["file_name"] == receipt['file_name']:
+#                     insert_record_by_group_id(
+#                         receipt, group_id, request_id, receipt['file_name'], image_file["base64"], image_file['hash'])
+#                     break
+
+#         # delete local storage
+#         files_name = []
+#         for image_file in upload_requests[request_id]["files"]:
+#             files_name.append(image_file["file_name"])
+#         threading.Thread(target=delete_parsed_data, args=(files_name,)).start()
+
+#         # upload image hash to db
+#         image_hashs = []
+#         for image_file in upload_requests[request_id]["files"]:
+#             image_hashs.append(image_file["hash"])
+#         if insert_image_hash(image_hashs) is False:
+#             return jsonify({"message": "Fail to insert image hash."})
+
+#         delete_parse_queue(request_id)
+#         temp_upload_request = upload_requests[request_id]
+#         del upload_requests[request_id]
+#         return jsonify({"message": "Image(s) processing complete.", "content": temp_upload_request})
+
+#     LOGGER.info("Upload complete.")
+#     return jsonify({"message": "Upload complete."})
+
+
+UPLOAD_REQUEST_LOCK = threading.Lock()
+
+
+class UploadRequests():
+    def __init__(self) -> None:
+        self.pool = {}
+        self.invalid_request_pool = []
+        self.image_hashs = []
+        self.pending_pool = {}
+        pass
+
+    def create(self, request_id):
+        try:
+            # ignore if request_id exsit.
+            self.pool[request_id]
+        except KeyError:
+            with UPLOAD_REQUEST_LOCK:
+                self.pool[request_id] = {
+                    "group_id": "", "total_files": "", "remaining": "", "files": []}
+
+    def initialize_values(self, request_id, group_id, total_files):
+        if self.pool[request_id]["group_id"] == "":
+            self.pool[request_id]["group_id"] = group_id
+
+        if self.pool[request_id]["total_files"] == "":
+            self.pool[request_id]["total_files"] = total_files
+            self.pool[request_id]["remaining"] = total_files
+
+    def insert_file(self, request_id, file_name, image_base64, image_hash):
+        try:
+            upload_request = self.pool[request_id]
+        except KeyError:
+            return None
+
+        upload_request["files"].append(
+            {"name": file_name, "base64": image_base64, "hash": image_hash})
+
+    def recived(self, request_id):
+        with UPLOAD_REQUEST_LOCK:
+            self.pool[request_id]["remaining"] -= 1
+
+    def mark_invalid(self, request_id):
+        self.pool[request_id]["is_valid"] = False
+
+    def drop(self, request_id):
+        with UPLOAD_REQUEST_LOCK:
+            self.invalid_request_pool.append(request_id)
+            del self.pool[request_id]
+
+    def is_valid_request(self, request_id):
+        try:
+            self.invalid_request_pool.index(request_id)
+        except ValueError:
+            try:
+                self.pool[request_id]
+                return True
+            except KeyError:
+                return False
+
+        return False
+
+    def find_request(self, request_id):
+        try:
+            return self.pool[request_id]
+        except KeyError:
+            return None
+
+    def remaining_files(self, request_id):
+        return self.pool[request_id]["remaining"]
+
+    def find_image_hash(self, image_hash):
+        try:
+            return self.image_hashs.index(image_hash)
+        except ValueError:
+            return None
+
+    def get_files(self, request_id):
+        return self.pool[request_id]["files"]
+
+    def get_files_name(self, request_id):
+        files_name = []
+        for image_file in self.pool[request_id]["files"]:
+            files_name.append(image_file["name"])
+
+        return files_name
+
+    def pending(self, request_id):
+        self.pending_pool.append(self.pool[request_id])
+
+    def update_receipts(self, request_id, receipts):
+        # create a dictionary mapping file names to receipts
+        receipt_dict = {r['file_name']: r for r in receipts}
+        for file_idx, image_file in enumerate(self.pool[request_id]["files"]):
+            if image_file["name"] in receipt_dict:
+                self.pool[request_id]["files"][file_idx]["receipt"] = receipt_dict[image_file["name"]]
+                del receipt_dict[image_file["name"]]
+
+    def get_pending_request(self, request_id):
+        return self.pending_pool[request_id]
+
+    def to_pending_pool(self, request_id):
+        self.pending_pool[request_id] = self.pool[request_id]
+        self.pending_pool["upload"] = False
+        del self.pool[request_id]
+
+    @staticmethod
+    def remove_base64(upload_request):
+        upload_request["files"] = [
+            {"hash": image_file["hash"], "receipt": image_file["receipt"]} for image_file in upload_request["files"]]
+        return upload_request
+
+
+upload_requests = UploadRequests()
+
+
+def validate_upload_request(request_id, group_id, total_files, file_name, image_file):
+
+    if not (re.match(UUID_REGEX, request_id) and upload_requests.is_valid_request(request_id)):
+        return "Invalid request id."
+
+    if not re.match(FILE_NAME_REGEX, file_name):
+        return "Invalid file name."
+
+    try:
+        total_files = int(total_files)
+    except ValueError:
+        return "Invalid total files number."
+    try:
+        Image.open(io.BytesIO(image_file))
+    except IOError:
+        return "Invalid file format."
+
+    records = get_group_records(group_id)
+    if records is None:
+        return "Invalid group id."
+
+    return ""
 
 
 @app.route('/test/upload/<string:group_id>', methods=['POST'])
 def handle_upload(group_id):
+
     # how many files are expected to be uploaded for this request_id
-    total_files = int(request.form.get('total_files'))
     request_id = request.form.get('request_id')
-
-    if request_id not in upload_requests:
-        upload_requests[request_id] = {
-            "request_id": request_id, "total_files": total_files, "remaining": total_files, "files": [], "hashs": []}
-
-    if not re.match(UUID_REGEX, request_id):
-        upload_requests[request_id]["remaining"] -= 1
-        return jsonify({"message": "Invalid request_id."}), 400
+    upload_requests.create(request_id)
+    total_files = request.form.get('total_files')
 
     image_file = request.files.get("file")
-
-    if not re.match(FILE_NAME_REGEX, image_file.filename):
-        upload_requests[request_id]["remaining"] -= 1
-        return jsonify({"message": "Invalid file name."}), 400
-
-    # image hash
     image_bytes = image_file.read()
-    try:
-        image = Image.open(io.BytesIO(image_bytes))
-    except IOError:
-        upload_requests[request_id]["remaining"] -= 1
-        return jsonify({"message": "Invalid image file."}), 400
 
+    error_message = validate_upload_request(
+        request_id, group_id, total_files, image_file.filename, image_bytes)
+    if error_message != "":
+        upload_requests.drop(request_id)
+        LOGGER.warning(error_message)
+        return jsonify({"message": error_message}), 400
+
+    upload_requests.initialize_values(request_id, group_id, int(total_files))
+    image = Image.open(io.BytesIO(image_bytes))
     image_hash = str(imagehash.average_hash(image))
-
-    if find_image_hash(image_hash) is not None:
-        upload_requests[request_id]["remaining"] -= 1
-        LOGGER.info("Duplicated image found.")
-        return jsonify({"message": "Duplicated image"}), 400
-
-    # convert image to base64
     image_base64 = base64.b64encode(image_bytes).decode('utf-8')
+
+    if upload_requests.find_image_hash(image_hash) is None:
+        # file already in DB
+        upload_requests.insert_file(
+            request_id, image_file.filename, image_base64, image_hash)
+        upload_requests.recived(request_id)
+    else:
+        upload_requests.recived(request_id)
+        return jsonify("Duplicated image."), 400
 
     # save image file to disk
     try:
@@ -213,56 +440,24 @@ def handle_upload(group_id):
             stream.write(base64.b64decode(image_base64))
     except Exception as e:
         LOGGER.error(e)
+        upload_requests.drop(request_id)
+        return jsonify({"message": "Server instenal error, please try to upload it again."}), 500
 
-    # append the file name and base64 to the current record
-    upload_requests[request_id]["files"].append({
-        "file_name": image_file.filename,
-        "base64": image_base64,
-        "hash": image_hash})
-
-    upload_requests[request_id]["remaining"] -= 1
-
-    # if no remaining files, execute receipt parser
-    if upload_requests[request_id]["remaining"] == 0:
-
-        # add records to parse_queue db
-        insert_parse_queue(
-            upload_requests[request_id]["request_id"],
-            group_id,
-            upload_requests[request_id]["files"],
-        )
-
-        parser_thread = execute_receipt_parser(request_id)
-        parser_thread.join()
+    if upload_requests.remaining_files(request_id) == 0:
+        # current still sequenize, but plan to do it async later.
+        files_name = upload_requests.get_files_name(request_id)
+        parser_output_string = execute_receipt_parser(files_name)
         receipts_json_string = parser_output_string.splitlines()[-1]
         receipts = json.loads(receipts_json_string)
 
-        # TODO: optmize
-        for image_file in upload_requests[request_id]["files"]:
-            for receipt in receipts:
-                if image_file["file_name"] == receipt['file_name']:
-                    insert_record_by_group_id(
-                        receipt, group_id, request_id, receipt['file_name'], image_file["base64"], image_file['hash'])
-                    break
+        upload_requests.update_receipts(request_id, receipts)
+        upload_requests.to_pending_pool(request_id)
 
-        # delete local storage
-        files_name = []
-        for image_file in upload_requests[request_id]["files"]:
-            files_name.append(image_file["file_name"])
-        threading.Thread(target=delete_parsed_data, args=(files_name,)).start()
+        response = upload_requests.get_pending_request(request_id)
+        response = upload_requests.remove_base64(response)
+        return jsonify(response)
 
-        # upload image hash to db
-        image_hashs = []
-        for image_file in upload_requests[request_id]["files"]:
-            image_hashs.append(image_file["hash"])
-        if insert_image_hash(image_hashs) is False:
-            return jsonify({"message": "Fail to insert image hash."})
-
-        delete_parse_queue(request_id)
-        del upload_requests[request_id]
-
-    LOGGER.info("Upload complete.")
-    return jsonify({"message": "Upload complete."})
+    return jsonify({"message": "Received", "file": image_file.filename})
 
 
 @ app.route('/internal/parse/<string:request_id>', methods=['GET', 'POST'])
